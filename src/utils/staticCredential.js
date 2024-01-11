e1ae458334cc89dc5441c964a694d29db032999d
tuannguyenbd <witvietnam@gmail.com>
2024-01-11 20:56:07 +0700

    first commit

---
+++ src/utils/staticCredential.js
@@ -0,0 +1,8 @@
+export const google_client_id = "498864605085-ajrm5m63s2uujdkd5g903osoho7poo53.apps.googleusercontent.com";
+export const fb_app_id = "926817608991029";
+export const appleLoginCredential = {
+  appleId: '',
+  serviceId: '',
+  privateKey: '',
+  redirectURI: ``
+}
